71798bdc8c1d82981cf42b2bd30bed406432d8fd
Michael Fix <mfix@wisc.edu>
2015-06-18 22:17:35 -0700

    first commit

---
+++ public_html/js/main.js
@@ -0,0 +1,17 @@
+//$("#ubercode").click(function(){
+//    var r = document.createRange();
+//    var w=$("#ubercode");
+//    r.selectNodeContents(w); 
+//    var sel=window.getSelection(); 
+//    sel.removeAllRanges(); 
+//    sel.addRange(r);
+//});
+$('[data-toggle="tooltip"]').tooltip()
+$('#ubercode').click(function(){
+    $('#ubercode').tooltip('show');
+    $('#lyftcode').tooltip('hide');
+});
+$('#lyftcode').click(function(){
+    $('#lyftcode').tooltip('show');
+    $('#ubercode').tooltip('hide');
+});
